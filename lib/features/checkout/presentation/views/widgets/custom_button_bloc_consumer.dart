60d2a670289c5f044e87cad8b49e42ee49f10b51
ma7moud332 <119896210+ma7moud332@users.noreply.github.com>
2025-05-29 17:43:33 +0300

    create amount model

--- lib/features/checkout/presentation/views/widgets/custom_button_bloc_consumer.dart
+++ lib/features/checkout/presentation/views/widgets/custom_button_bloc_consumer.dart
@@ -1,9 +1,12 @@
-import 'package:check_out_app/features/checkout/data/models/payment_intent_input_model.dart';
+import 'dart:developer';
+
 import 'package:check_out_app/features/checkout/presentation/manager/cubit/payment_cubit.dart';
 import 'package:check_out_app/features/checkout/presentation/views/thank_you_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_paypal_payment/flutter_paypal_payment.dart';
 
+import '../../../../../core/utils/api_keys.dart';
 import '../../../../../core/widgets/custom_button.dart';
 
 class CustomButtonBlocConsumer extends StatelessWidget {
@@ -31,16 +34,81 @@ class CustomButtonBlocConsumer extends StatelessWidget {
       builder: (context, state) {
         return CustomButton(
           onTap: () {
-            PaymentIntentInputModel paymentIntentInputModel =
-                PaymentIntentInputModel(
-                  amount:
-                      '100', // Amount in cents (e.g., 1050 EGP = 105000 cents)
-                  currency: 'USD',
-                  customerId: 'cus_SNsA0bmLFA6K3n',
-                );
-            BlocProvider.of<PaymentCubit>(
-              context,
-            ).makePayment(paymentIntentInputModel: paymentIntentInputModel);
+            // PaymentIntentInputModel paymentIntentInputModel =
+            //     PaymentIntentInputModel(
+            //       amount:
+            //           '100', // Amount in cents (e.g., 1050 EGP = 105000 cents)
+            //       currency: 'USD',
+            //       customerId: 'cus_SNsA0bmLFA6K3n',
+            //     );
+            // BlocProvider.of<PaymentCubit>(
+            //   context,
+            // ).makePayment(paymentIntentInputModel: paymentIntentInputModel);
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (BuildContext context) => PaypalCheckoutView(
+                  sandboxMode: true,
+                  clientId: 'cus_SNsA0bmLFA6K3n',
+                  secretKey: ApiKeys.secretKey,
+                  transactions: const [
+                    {
+                      "amount": {
+                        "total": "100",
+                        "currency": "USD",
+                        "details": {
+                          "subtotal": "100",
+                          "shipping": "0",
+                          "shipping_discount": 0,
+                        },
+                      },
+                      "description": "The payment transaction description.",
+
+                      "item_list": {
+                        "items": [
+                          {
+                            "name": "Apple",
+                            "quantity": 4,
+                            "price": "10",
+                            "currency": "USD",
+                          },
+                          {
+                            "name": "Pineapple",
+                            "quantity": 5,
+                            "price": "12",
+                            "currency": "USD",
+                          },
+                        ],
+
+                        // Optional
+                        //   "shipping_address": {
+                        //     "recipient_name": "Tharwat samy",
+                        //     "line1": "tharwat",
+                        //     "line2": "",
+                        //     "city": "tharwat",
+                        //     "country_code": "EG",
+                        //     "postal_code": "25025",
+                        //     "phone": "+00000000",
+                        //     "state": "ALex"
+                        //  },
+                      },
+                    },
+                  ],
+                  note: "Contact us for any questions on your order.",
+                  onSuccess: (Map params) async {
+                    log("onSuccess: $params");
+                    Navigator.pop(context);
+                  },
+                  onError: (error) {
+                    log("onError: $error");
+                    Navigator.pop(context);
+                  },
+                  onCancel: () {
+                    print('cancelled:');
+                    Navigator.pop(context);
+                  },
+                ),
+              ),
+            );
           },
           isLoading: state is PaymentLoading ? true : false,
           text: 'Continue',
